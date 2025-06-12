6aa6960e6170f8276f0ae61b952c296d0f59ab99
Capoo <nhatranthanh113@gmail.com>
2025-06-12 08:44:13 +0700

    Initial Commit

---
+++ Millionaire/MillionaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  MillionaireApp.swift
+//  Millionaire
+//
+//  Created by Thanh Nhã on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct MillionaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
